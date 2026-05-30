8a62ab8aebd778087aa52eeaa7fd304419ddef73
lzffzl13 <18671210313@163.com>
2026-05-30 19:27:49 +0800

    完成数据库迁移

--- app/main.py
+++ app/main.py
@@ -6,14 +6,13 @@ from app.api.article import router as article_router
 from app.api.auth import router as auth_router
 from app.api.user import router as user_router
 from app.core.logging import setup_logging
-from app.db.session import Base, engine
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # 启动时执行
     setup_logging()
-    Base.metadata.create_all(bind=engine)
+
     yield
     # 关闭时执行
 
